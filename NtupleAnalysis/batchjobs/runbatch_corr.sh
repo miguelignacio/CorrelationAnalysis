43c6ae83bacacb69c3c523143b35896c4d1f725a
Fernando Torales - Acosta <fernando@pdsf8.nersc.gov>
2018-07-24 17:01:09 -0700

    friendlier correlation script

--- NtupleAnalysis/batchjobs/runbatch_corr.sh
+++ NtupleAnalysis/batchjobs/runbatch_corr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# -eq 1 ]] ; then
-    echo 'please give [13d,13e, or 13f] and [Track Skim GeV] as arguments'
+    echo 'please give [13d, 13e, 17q,...] and [MinBias.hdf5] [test/full] as arguments'
     exit 0
 fi
 
@@ -10,19 +10,23 @@ date
 module add ROOT/6.08.00
 
 #for p in {0,4,6}
-for p in $2
+for p in 0
 do
-    if [ ! -f ../InputData/$1_$2GeVTrack_paired.root ]; then
+    name=$(echo "$filename" | cut -f 1 -d '.')
+    if [ ! -f $name_${p}GeVTrack_paired.root ]; then
 	echo "calling paired Injector with file $1.root and track Gev $p"
-	./../pair_gale_shapley/paired_injector ../InputData/$1.root $p
+	./../pair_gale_shapley/paired_injector $1.root $p
     fi
     
     for i in {0..280..20} #Mix 300 events
     do
 	mix_min=$i
 	mix_max="$((i + 19))"
-	#sbatch -p shared-chos -t 16:00:00 runCorr.sh $1 $mix_min $mix_max $p
-	./runCorr.sh $1 $mix_min $mix_max $p
+	if [[ $3 == full ]]; then
+	    sbatch -p shared-chos -t 16:00:00 runCorr.sh $1_${p}GeVTrack_paired.root $2 $mix_min $mix_max $p	  
+	else
+	./runCorr.sh $1_${p}GeVTrack_paired.root $2 $mix_min $mix_max $p
+	fi
     echo "$mix_min $mix_max $1"
     done
 done
