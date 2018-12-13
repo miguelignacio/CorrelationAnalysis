4b1b933f27a7fba4401f3dc8e74211d782d141c9
Fernando <fernando@Fernandos-MacBook-Pro.local>
2018-12-12 17:10:19 -0800

    Diviision macro went back to SR and BR division after merge. Divides by Inclusive now

--- NtupleAnalysis/Divide_Same_Mix.cc
+++ NtupleAnalysis/Divide_Same_Mix.cc
@@ -34,9 +34,9 @@ int main(int argc, char *argv[])
   int nTrackSkims = 1;
   float* trackPtSkims;
   trackPtSkims = new float[nTrackSkims+1];
-  trackPtSkims[0] = 0.0; trackPtSkims[1] = 40.0;
-  // trackPtSkims[1] = 4.0; //trackPtSkims[2] = 6.0;
-  // trackPtSkims[2] = 40.0;
+  trackPtSkims[0] = 0.0; trackPtSkims[1] = 30.0;
+  // trackPtSkims[1] = 4.0; trackPtSkims[2] = 6.0;
+  //trackPtSkims[2] = 30.0;
 
   TFile* MixFile[nTrackSkims];
   for (int iSkim = 0; iSkim < nTrackSkims; iSkim ++){
@@ -44,10 +44,10 @@ int main(int argc, char *argv[])
     //MixFile[iSkim] = TFile::Open(Form("Mix_Correlation_%1.0fGeVTracks_13defv1_ALL.root",trackPtSkims[iSkim]));
     //MixFile[iSkim] = TFile::Open(Form("13d_4L_GMB_Correlation_%1.0fGeVTracks.root",trackPtSkims[iSkim]));
     //MixFile[iSkim] = TFile::Open(Form("%s_%1.0fGeVTracks.root",basic_name.c_str(),trackPtSkims[iSkim]));
-    //MixFile[iSkim] = TFile::Open(Form("InputData/17q_MB_%1.0fGeV_15_20.root",trackPtSkims[iSkim]));
+
     MixFile[iSkim] = TFile::Open((TString)argv[2]);
-    //MixFile[iSkim] = TFile::Open("InputData/17q_MB_4GeVTrack_Correlation_12_15.root");
-    std::cout<<Form("%s_%1.0fGeVTracks.root",basic_name.c_str(),trackPtSkims[iSkim])<<std::endl;
+
+    //std::cout<<Form("%s_%1.0fGeVTracks.root",basic_name.c_str(),trackPtSkims[iSkim])<<std::endl;
     if (MixFile[iSkim] == NULL) {
       std::cout<<MixFile[iSkim]<<std::endl;
       std::cout << " Mixed file "<<iSkim<<" failed" << std::endl;
@@ -55,7 +55,7 @@ int main(int argc, char *argv[])
   }
 
 
-  FILE* config = fopen("Corr_config.yaml", "r");
+  FILE* config = fopen("Corr_config2.yaml", "r");
 
   int n_eta_bins = 0;
   int n_phi_bins = 0;
@@ -187,7 +187,7 @@ int main(int argc, char *argv[])
 	  1,ptbins[ipt],ptbins[ipt+1],100*ztbins[izt],100*ztbins[izt+1]));
 
       DNN1_entries = Same_DNN1_Corr[izt+ipt*nztbins]->GetEntries();
-      fprintf(stderr, "%s:%d: Number of Entries in Same DNN1 %lu \n",__FILE__,__LINE__,DNN1_entries);
+      fprintf(stderr, "%s:%d: Number of Entries in Same DNN1 %lld \n",__FILE__,__LINE__,DNN1_entries);
       
       if (Same_DNN1_Corr[izt+ipt*nztbins] == NULL) {
 	std::cout << "Same 1 TH2D fail" << std::endl;
@@ -220,7 +220,7 @@ int main(int argc, char *argv[])
 		  ptbins[ipt],ptbins[ipt+1],100*ztbins[izt],100*ztbins[izt+1]);
 
 	  DNN1_entries = Mix_Inclusive_Corr[izt+ipt*nztbins]->GetEntries();
-	  fprintf(stderr, "%s:%d: Number of Entries in Mix Inclusive %lu \n",__FILE__,__LINE__,DNN1_entries);
+	  fprintf(stderr, "%s:%d: Number of Entries in Mix Inclusive %lld \n",__FILE__,__LINE__,DNN1_entries);
       
 	  if (Mix_Inclusive_Corr[izt+ipt*nztbins] == NULL) {
 	    std::cout << "Mix Incl TH2D fail" << std::endl;
@@ -245,8 +245,6 @@ int main(int argc, char *argv[])
 	    std::cout << " mix TH2D DNN2 read fail "<<"Mix file "<<iSkim<<std::endl;
 	    exit(EXIT_FAILURE);}     
 	}
-	else
-	  fprintf(stderr,"%s: %d: Could not place Track pT bin in mixed File",__FILE__,__LINE__);
 
       }
     //Normalize MixOAing to 1 at ∆eta∆phi = 0,0
@@ -275,8 +273,11 @@ int main(int argc, char *argv[])
 
     Mix_Inclusive_Corr[izt+ipt*nztbins]->Scale(1.0/mix_Inclusive_intgrl);
     //Mix_Inclusive_Corr[izt+ipt*nztbins]->Scale(1.0/300);
-    Mix_DNN1_Corr[izt+ipt*nztbins]->Scale(1.0/mix_DNN1_intgrl);
-    Mix_DNN2_Corr[izt+ipt*nztbins]->Scale(1.0/mix_DNN2_intgrl);
+//     Mix_DNN1_Corr[izt+ipt*nztbins]->Scale(1.0/mix_DNN1_intgrl);
+//     Mix_DNN2_Corr[izt+ipt*nztbins]->Scale(1.0/mix_DNN2_intgrl);
+
+    Mix_DNN1_Corr[izt+ipt*nztbins]->Scale(1.0/mix_Inclusive_intgrl);
+    Mix_DNN2_Corr[izt+ipt*nztbins]->Scale(1.0/mix_Inclusive_intgrl);
 
     std::cout<<mix_DNN1_intgrl<<std::endl;
 
@@ -285,8 +286,8 @@ int main(int argc, char *argv[])
     //DIVIDE MIXING
     Same_Inclusive_Corr[izt+ipt*nztbins]->Divide(Mix_Inclusive_Corr[izt+ipt*nztbins]);
 
-    //Same_DNN1_Corr[izt+ipt*nztbins]->Divide(Mix_DNN1_Corr[izt+ipt*nztbins]);
-    //Same_DNN2_Corr[izt+ipt*nztbins]->Divide(Mix_DNN2_Corr[izt+ipt*nztbins]);
+//     Same_DNN1_Corr[izt+ipt*nztbins]->Divide(Mix_DNN1_Corr[izt+ipt*nztbins]);
+//     Same_DNN2_Corr[izt+ipt*nztbins]->Divide(Mix_DNN2_Corr[izt+ipt*nztbins]);
 
     Same_DNN1_Corr[izt+ipt*nztbins]->Divide(Mix_Inclusive_Corr[izt+ipt*nztbins]);
     Same_DNN2_Corr[izt+ipt*nztbins]->Divide(Mix_Inclusive_Corr[izt+ipt*nztbins]);
